ea5f5070279afed5fb2a0a419db11a040e359eb9
Josue Mullo <josue.mullo@udla.edu.ec>
2025-06-15 16:15:15 -0500

    Añadiendo mi nombre en el encabezado de aplicaccion en AppShell.xaml

--- Notes/Models/About.cs
+++ Notes/Models/About.cs
@@ -3,7 +3,7 @@
 internal class About
 {
     public string Title => "Josue Mullo";
-    public string Version => "Estudiante de Ingeniería de Software";
+    public string Version => "1.0";
     public string MoreInfoUrl => "https://github.com/AndresUdla";
-    public string Message => "Soy Andrés, un apasionado por la programación, actualmente aprendiendo Java, .NET MAUI y más. Me gusta aprender cosas nuevas cada día y desarrollar ideas útiles con tecnologías modernas.";
+    public string Message => "Hola, soy estudiante de Ingenieria de Software y un apasionado por la programación, actualmente aprendiendo Java, .NET MAUI y más. Me gusta aprender cosas nuevas cada día y desarrollar ideas útiles con tecnologías modernas.";
 }
